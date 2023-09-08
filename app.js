a69b57aa823381b1c953355c3fe2c0ef0a6fa9f2
mahmoud <mahmoud123@gmail.com>
2023-09-08 16:08:22 +0300

    change images

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const progress_bar = document.querySelectorAll(".progress-bar");
-const skillsSection = document.getElementById("skills");
-    progress_bar.forEach((el) => {
-      el.style.width = el.getAttribute("data_width");
-    });
-// window.onscroll = () => {
-//   if (window.scrollY >= skillsSection.offsetTop) {
-//     progress_bar.forEach((el) => {
-//       el.style.width = el.getAttribute("data_width");
-//     });
-//   } else {
-//     progress_bar.forEach((el) => {
-//       el.style.width = 0;
-//     });
-//   }
-// };
+// const skillsSection = document.getElementById("skillsSection");
+
+progress_bar.forEach((el) => {
+  el.style.width = el.attributes.data_width.value;
+});
+
+
