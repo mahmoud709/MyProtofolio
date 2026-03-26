53feb8828e7688b81d89b5f17610476a5573463c
mahmoud <mahmoudshalaby363@gmail.com>
2026-03-26 21:06:01 +0200

    feat:redesign my protofolio

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ progress_bar.forEach((el) => {
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {  // When the screen width is 0px or larger
+        slidesPerView: 1,  // Show 1 slide on small screens
+        spaceBetween: 10,  // Adjust space between slides for small screens
+      },
+      768: {  // When the screen width is 768px or larger
+        slidesPerView: 2,  // Show 2 slides on medium screens
+        spaceBetween: 20,
+      },
+      1024: {  // When the screen width is 1024px or larger
+        slidesPerView: 3,  // Show 3 slides on large screens
+        spaceBetween: 30,
+      },
+    },
+  });
+});
